94a5456b65c4aef62543b3f4d5aa5ca4ab1026d4
Snowcat <105678773+Snowcat12@users.noreply.github.com>
2023-03-27 21:35:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let upload = document.getElementById('upload');
+
+let outputBx = document.getElementById('outputBx');
+
+upload.addEventListener('change', ()=>{
+    let fr = new FileReader();
+
+    fr.readAsText(upload.files[0]);
+
+    fr.onload = function(){
+        outputBx.innerHTML = fr.result;
+    }
+});
